4f33ec228b681c28b8d894d09ca95be838f9d695
Илья Соловьев <solovevilya3101@gmail.com>
2024-01-30 14:53:26 +0200

    Add routes

--- config/routes.php
+++ config/routes.php
@@ -2,5 +2,8 @@
 
 use Core\Routing\Router;
 
-Router::get('/', [new Up\Controllers\IndexController(), 'indexAction']);
+Router::get('/main/:tag/', [new Up\Controllers\IndexController(), 'indexAction']);
 Router::get('/product/:id/', [new Up\Controllers\DetailController(), 'detailsAction']);
+Router::get('/admin/:id/', [new Up\Controllers\AdminController(), 'adminAction']);
+Router::get('/order/:id/', [new Up\Controllers\OrderController(), 'orderAction']);
+Router::get('/auth/', [new Up\Controllers\AuthController(), 'authAction']);
---
+++ src/Controllers/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Up\Controllers;
+
+class AuthController extends BaseController
+{
+	public function authAction(): string
+	{
+		return "auth page ";
+	}
+}
