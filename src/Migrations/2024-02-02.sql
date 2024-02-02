e93adac8a8b0541398f474bef1894c486f7b59e2
Илья Соловьев <solovevilya3101@gmail.com>
2024-02-02 13:24:47 +0200

    Add a migration script for loading images from the database

---
+++ src/Migrations/2024-02-02.sql
@@ -0,0 +1,13 @@
+SET NAMES utf8mb4;
+
+INSERT INTO `IMAGE` (`ID`, `PRODUCT_ID`, `PATH`, `IS_COVER`)
+VALUES (1, 1, '1.jpg', 1),
+       (2, 2, '2.webp', 1),
+       (3, 3, '3.png', 1),
+       (4, 4, '4.png', 1),
+       (5, 5, '5.png', 1),
+       (6, 6, '6.png', 1),
+       (7, 7, '7.webp', 1),
+       (8, 8, '8.webp', 1),
+       (9, 9, '9.jpg', 1),
+       (10, 10, '10.png', 1);
