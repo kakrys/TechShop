642a34a5af0dba66243167ba53246b1937bc4efa
Yulya Gaus <yulya.yulya.010202@gmail.com>
2024-02-04 16:58:52 +0300

    Reformat success page

--- src/Views/default/pages/success.php
+++ src/Views/default/pages/success.php
@@ -7,9 +7,12 @@
 <div class="orderModal" data-field-message="CODE:200" >
 	<div class="orderModal__container">
 		<div class="orderStatus">
-			<img src="/assets/images/common/tick-circle.png" alt="Successful order submit" class="orderStatus__img">
-			<p class="orderStatus__text" <?=!empty($orderErrors)?'style="color: red"':''?>>Successfully Ordered</p>
+			<img src="/assets/images/common/<?=!empty($orderErrors)?'close-circle':'tick-circle'?>.png" alt="order status image" class="orderStatus__img">
+			<p class="orderStatus__text" <?=!empty($orderErrors)?'style="color: #C91433;"':''?>>
+				<?=!empty($orderErrors)?'Oops. <br> <span>Unfortunately, there was a problem during<br>creating your order. try again later.</span>':'Successfully Ordered'?>
+			</p>
 		</div>
 		<a href="/" class="orderModal__btn">Return To Main Page</a>
 	</div>
 </div>
+<div class="emptyForBlur"></div>
