39b75584e134ed426b3e9f66289d5ebba0d8d9aa
Yulya Gaus <yulya.yulya.010202@gmail.com>
2024-02-11 16:24:09 +0300

    Reformat login.js, add media styles for login page

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -10,3 +10,11 @@ else
 {
 	form.classList.remove("show__signUp");
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+	const footer = document.querySelector(".footer");
+	if (currentPage === '/login/' || currentPage === '/registration/')
+	{
+		footer.style.display = 'none';
+	}
+});
