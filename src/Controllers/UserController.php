2f028b8869ff3dd390534dabd04cdc82ed846389
Илья Соловьев <solovevilya3101@gmail.com>
2024-02-11 17:46:28 +0200

    Added display of information about user orders

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -1,10 +1,12 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Up\Controllers;
 
 use Core\Http\Request;
 use Exception;
+use Up\Services\Repository\OrderService;
 use Up\Services\Repository\UserService;
 
 class UserController extends BaseController
@@ -12,28 +14,33 @@ class UserController extends BaseController
 	/**
 	 * @throws Exception
 	 */
-	public function userAction():string
+	public function userAction(): string
 	{
 		session_start();
 		if (isset($_SESSION['UserEmail']))
 		{
 			$user = UserService::getUserByEmail($_SESSION['UserEmail']);
+			$orders = OrderService::getOrderList($_SESSION['UserEmail']);
+
 			$params = [
 				'userEmail' => $user->email,
 				'user' => $user,
 				'userFullName' => $user->name . ' ' . $user->surname,
+				'orders' => $orders,
 			];
+
 			return $this->render('account', $params);
 		}
 
 		header('Location: /login/');
 	}
+
 	public function updateInfoAction(): void
 	{
 		session_start();
 		$request = Request::getBody();
 		$arrayKey = array_key_first($request);
-		$updateField = mb_substr($arrayKey,3);
+		$updateField = mb_substr($arrayKey, 3);
 		$funcName = 'updateUser' . $updateField;
 		UserService::$funcName();
 		header('Location: /account/');
