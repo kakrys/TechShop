12f37e8415959ee4feadb18d05c53afd32b184e8
Илья Соловьев <solovevilya3101@gmail.com>
2024-02-11 17:46:28 +0200

    Added display of information about user orders

--- src/Views/default/components/account-orders.php
+++ src/Views/default/components/account-orders.php
@@ -1,91 +1,37 @@
 <?php
-
+/**
+ * @var Up\Models\Order[] $orders
+ * */
 ?>
 
 <div class="title__container">
 	<h1 class="account__title">Order History</h1>
 </div>
 <div class="user__ordersContainer">
+	<?php foreach ($orders as $order):?>
 	<div class="userOrder">
 		<ul class="user__ordersInfoList">
 			<li class="user__ordersInfoItem">
 				<h4 class="user__ordersInfoTitle">order code</h4>
-				<p class="user__ordersInfoSubtitle">#8967856</p>
+				<p class="user__ordersInfoSubtitle">#<?=$order->id?></p>
 			</li>
 			<li class="user__ordersInfoItem">
-				<h4 class="user__ordersInfoTitle">Order status</h4>
-				<p class="user__ordersInfoSubtitle">status 200 =)</p>
-			</li>
-			<li class="user__ordersInfoItem">
-				<h4 class="user__ordersInfoTitle">Placed on</h4>
-				<p class="user__ordersInfoSubtitle">2023/08/20</p>
-			</li>
-			<li class="user__ordersInfoItem">
-				<h4 class="user__ordersInfoTitle">Total</h4>
-				<p class="user__ordersInfoSubtitle">$10,998.00</p>
-			</li>
-			<li class="user__ordersInfoItem">
-				<h4 class="user__ordersInfoTitle">Sent to</h4>
-				<p class="user__ordersInfoSubtitle">Jimmy Smith</p>
-			</li>
-		</ul>
-		<ul class="user__ordersImagesList">
-			<li class="user__ordersImagesItem">
-				<img src="/assets/images/products/laptop/macOS.png" alt="your orders picture" class="account__ordersImage">
-			</li>
-			<li class="user__ordersImagesItem">
-				<img src="/assets/images/products/audio/airpods2.png" alt="your orders picture" class="account__ordersImage">
-			</li>
-			<li class="user__ordersImagesItem">
-				<img src="/assets/images/products/phones/iphone14Pro.png" alt="your orders picture" class="account__ordersImage">
-			</li>
-			<li class="user__ordersImagesItem">
-				<img src="/assets/images/products/tablets/ipadPro12.png" alt="your orders picture" class="account__ordersImage">
-			</li>
-			<li class="user__ordersImagesItem">
-				<img src="/assets/images/products/watches/applewatchBlue.png" alt="your orders picture" class="account__ordersImage">
-			</li>
-		</ul>
-	</div>
-	<div class="userOrder">
-		<ul class="user__ordersInfoList">
-			<li class="user__ordersInfoItem">
-				<h4 class="user__ordersInfoTitle">order code</h4>
-				<p class="user__ordersInfoSubtitle">#8967856</p>
-			</li>
-			<li class="user__ordersInfoItem">
-				<h4 class="user__ordersInfoTitle">Order status</h4>
-				<p class="user__ordersInfoSubtitle">status 200 =)</p>
+				<h4 class="user__ordersInfoTitle">Product</h4>
+				<p class="user__ordersInfoSubtitle"><?=$order->productTitle?></p>
 			</li>
 			<li class="user__ordersInfoItem">
 				<h4 class="user__ordersInfoTitle">Placed on</h4>
-				<p class="user__ordersInfoSubtitle">2023/08/20</p>
+				<p class="user__ordersInfoSubtitle"><?=$order->dataCreate?></p>
 			</li>
 			<li class="user__ordersInfoItem">
 				<h4 class="user__ordersInfoTitle">Total</h4>
-				<p class="user__ordersInfoSubtitle">$10,998.00</p>
+				<p class="user__ordersInfoSubtitle">$<?=$order->price?></p>
 			</li>
 			<li class="user__ordersInfoItem">
 				<h4 class="user__ordersInfoTitle">Sent to</h4>
-				<p class="user__ordersInfoSubtitle">Jimmy Smith</p>
-			</li>
-		</ul>
-		<ul class="user__ordersImagesList">
-			<li class="user__ordersImagesItem">
-				<img src="/assets/images/products/laptop/macOS.png" alt="your orders picture" class="account__ordersImage">
-			</li>
-			<li class="user__ordersImagesItem">
-				<img src="/assets/images/products/audio/airpods2.png" alt="your orders picture" class="account__ordersImage">
-			</li>
-			<li class="user__ordersImagesItem">
-				<img src="/assets/images/products/phones/iphone14Pro.png" alt="your orders picture" class="account__ordersImage">
-			</li>
-			<li class="user__ordersImagesItem">
-				<img src="/assets/images/products/tablets/ipadPro12.png" alt="your orders picture" class="account__ordersImage">
-			</li>
-			<li class="user__ordersImagesItem">
-				<img src="/assets/images/products/watches/applewatchBlue.png" alt="your orders picture" class="account__ordersImage">
+				<p class="user__ordersInfoSubtitle"><?=$order->userName . ' ' . $order->userSurname?></p>
 			</li>
 		</ul>
 	</div>
+	<?php endforeach;?>
 </div>
