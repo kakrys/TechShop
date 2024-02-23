79a82b5ca3b109f94b9a623a49d01c239a42458e
Илья Соловьев <solovevilya3101@gmail.com>
2024-02-23 12:57:59 +0200

    Fixing a bug on the checkout page

--- src/Views/default/pages/order.php
+++ src/Views/default/pages/order.php
@@ -6,6 +6,7 @@
  */
 
 use Up\Services\SecurityService;
+var_dump($user->address);
 
 ?>
 <div class="wrapper orderTitleContainer">
@@ -14,11 +15,11 @@ use Up\Services\SecurityService;
 <div class="wrapper order">
 	<div class="order__formContainer">
 		<form class="order__form" action="/success/" method="post">
-			<input class="order__input" id="userName" type="hidden" name="id" value=<?=SecurityService::safeString($user->id)?> pattern="^[^\s]+(\s.*)?$" required>
+			<input class="order__input" id="userName" type="hidden" name="id" value="<?=SecurityService::safeString($user->id)?>" pattern="^[^\s]+(\s.*)?$" required>
 			<label class="order__label" for="userEmail">E-mail</label>
-			<input class="order__input" id="userEmail" type="email" name="email" value=<?=SecurityService::safeString($user->email)?> pattern="^[^\s]+(\s.*)?$" required>
+			<input class="order__input" id="userEmail" type="email" name="email" value="<?=SecurityService::safeString($user->email)?>" pattern="^[^\s]+(\s.*)?$" required>
 			<label class="order__label" for="userAddress">Ship to</label>
-			<input class="order__input" id="userAddress" type="text" name="address" value=<?=SecurityService::safeString($user->address)?> pattern="^[^\s]+(\s.*)?$" required>
+			<input class="order__input" id="userAddress" type="text" name="address" value="<?=SecurityService::safeString($user->address)?>" pattern="^[^\s]+(\s.*)?$" required>
 			<input name="productID" type="hidden" value="<?=SecurityService::safeString($product->getId())?>">
 			<input name="productPrice" type="hidden" value="<?=SecurityService::safeString($product->getPrice())?>">
 			<button class="order__addOrder" type="submit">Submit & Order</button>
